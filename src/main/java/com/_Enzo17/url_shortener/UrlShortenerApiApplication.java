3b666d723e4ab170cc823e5e377756b2c6c64d12
21Enzo17 <129308114+21Enzo17@users.noreply.github.com>
2025-01-23 17:05:48 -0300

    first commit

---
+++ src/main/java/com/_Enzo17/url_shortener/UrlShortenerApiApplication.java
@@ -0,0 +1,13 @@
+package com._Enzo17.url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_Enzo17/url_shortener/UrlShortenerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com._Enzo17.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
